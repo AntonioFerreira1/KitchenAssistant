b44d1aa8f834823731877067d312f1b15d27faa0
AntonioFerreira1 <ada.ferreira@campus.fct.unl.pt>
2022-11-22 19:45:14 +0000

    Login screen added

--- app/src/main/java/com/example/kitchenassistant/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/kitchenassistant/ui/account/AccountFragment.kt
@@ -1,5 +1,6 @@
 package com.example.kitchenassistant.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.kitchenassistant.MainActivity
 import com.example.kitchenassistant.databinding.FragmentAccountBinding
+import com.example.kitchenassistant.ui.login.LoginActivity
 
 class AccountFragment : Fragment() {
 
@@ -22,16 +25,14 @@ class AccountFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(AccountViewModel::class.java)
-
         _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        //val textView: TextView = bindin
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            //textView.text = it
+
+        binding.button2.setOnClickListener {
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
         }
+
         return root
     }
 
